2c31fb67462707bc09e2d85c43896f96790274ce
Adam McChesney <amcchesney@taos.com>
2019-09-23 15:05:51 -0500

    update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,21 +1,2 @@
 python --version
-PROJECT="harvest-reporting"
-
-CODE_HOME=~/Builds/$PROJECT/code
-PYENV_HOME=~/Builds/$PROJECT/python
-export PYENV_HOME
-export PYTHONPATH=""
-echo "Creating new Python env"
-/usr/local/bin/python3 -m venv  --clear $PYENV_HOME
-source $PYENV_HOME/bin/activate
-
-echo "Copy Project"
-mkdir -p ~/Builds
-mkdir -p $CODE_HOME
-cp -a . $CODE_HOME
-cd $CODE_HOME
-
-pip install --upgrade pip
-pip install nose
-pip install coverage
-pip install -r requirements.txt
+pip install -r devrequirements.txt --user

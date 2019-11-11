e401c980a1c2ca79c2fb61d6f9514443a284ccee
Adam McChesney <amcchesney@taos.com>
2019-11-11 16:19:54 -0600

    update conf.py
    removing string interpol

--- .docs/conf.py
+++ .docs/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath(".."))
 # -- Project information -----------------------------------------------------
 project = "Harvest Reporting"
 author = "Austin and Adam"
-copyright = f"2019, {author}"
+copyright = "2019, Austin and Adam"
 
 # The full version, including alpha/beta/rc tags
 release = "0.0.1"

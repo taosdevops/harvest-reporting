a30ed37fe297f5fbf9d5b5152ef103f8321337d0
Robert R. Meyer <blue.dog.archolite@gmail.com>
2020-04-07 08:36:08 -0600

    Documents Are up to date with code

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,17 +13,18 @@
 import os
 import sys
 import sphinx_rtd_theme
-sys.path.insert(0, os.path.abspath('../..'))
+
+sys.path.insert(0, os.path.abspath("../.."))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'Harvest Reporting'
-copyright = '2019, Austin Earl, Adam McChesney'
-author = 'Austin Earl, Adam McChesney'
+project = "Harvest Reporting"
+copyright = "2019, Austin Earl, Adam McChesney, Robert R. Meyer"
+author = "Austin Earl, Adam McChesney, Robert R. Meyer"
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = "0.1.1"
 
 
 # -- General configuration ---------------------------------------------------
@@ -41,7 +42,7 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -54,9 +55,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]

e75b06e25be7ea8f8acfda08cd56512c0cb5a499
Adam McChesney <amcchesney@taos.com>
2020-04-24 16:54:26 -0500

    refactor effort
    NEW FEATURE
    if you wish to run through tests for only a specific client
    a new client field client filter is now available.
    its a string array and will suppress all messages
    that arent targetting a client in the list
    
    REFACTOR
    pushed email sending to go through same general process
    of channel posts to ensure simplified exception handling
    and a single array to house notification targets

--- hreporting/emails.py
+++ hreporting/emails.py
@@ -1,59 +1,35 @@
 import logging
 from datetime import date
-
+import sendgrid
 from sendgrid.helpers.mail import *
+from hreporting import config
 
 logging.getLogger("harvest_reports")
 
+SENDGRID_CLIENT = sendgrid.SendGridAPIClient(api_key=config.SENDGRID_API_KEY)
 
 class SendGridSummaryEmail:
     def __init__(
         self,
-        client_name: str,
-        emails: list,
-        left: float,
-        percent: float,
-        sg_client,
-        used: float,
-        from_email: str,
+        sg_client=SENDGRID_CLIENT,
+        from_email: str=config.ORIGIN_EMAIL_ADDRESS,
     ):
-
-        self.client_name = client_name
-        self.emails = map(To, emails)
-        self.left = left
-        self.percent = percent
         self.sg_client = sg_client
-        self.used = used
         self.from_email = from_email
 
-    def email_body(self) -> str:
-        hour_report_template = """
-        Client:           {name}
-        Used Hours:       {used}
-        Remaining Hours:  {left}
-        Percent:          {percent}
-        """
-        return str.format(
-            hour_report_template,
-            name=self.client_name,
-            used=self.used,
-            left=self.left,
-            percent="%d%%" % (self.percent),
-        )
-
-    def construct_mail(self) -> Mail:
+    def construct_mail(self, emails, client_name:str, body) -> Mail:
         current_date = date.today().strftime("%B %d, %Y")
         subject = Subject(
-            f"{self.client_name} usage of DevOps Now hours as of {current_date} "
+            f"{client_name} usage of DevOps Now hours as of {current_date} "
         )
 
-        content = Content("text/plain", self.email_body())
+        content = Content("text/plain", body)
 
-        return Mail(Email(self.from_email), list(self.emails), subject, content)
+        return Mail(Email(self.from_email), list(emails), subject, content)
 
-    def email_send(self) -> dict:
-        if self.emails:
-            mail = self.construct_mail()
+    def email_send(self, emails, client_name:str, body) -> dict:
+        if emails:
+            mail = self.construct_mail(emails, client_name, body)
 
             response = self.sg_client.client.mail.send.post(request_body=mail.get())
 

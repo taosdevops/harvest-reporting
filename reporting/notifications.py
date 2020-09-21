d6ce50e17641f063ba1771e4a89e965773f343f9
Gretchen Shelby-Dormer <gshelbydormer@taos.com>
2020-09-21 09:57:53 -0400

    Change relative import to absolute (properly)

--- reporting/notifications.py
+++ reporting/notifications.py
@@ -11,7 +11,7 @@ from taosdevopsutils.slack import Slack
 from harvestapi.customer import HarvestCustomer
 from sendgridapi.emails import SendGridSummaryEmail
 
-from .config import Recipients
+from reporting.config import Recipients
 
 LOGGER = logging.getLogger(__name__)
 

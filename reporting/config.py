f30c517526868c55f9cfaa77280037804c4d331a
root <root@LT-GSHELBYDORMER.taos.local>
2020-11-09 11:53:36 -0500

    akhjgleg

--- reporting/config.py
+++ reporting/config.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import os
 from dataclasses import dataclass, field
@@ -34,7 +35,7 @@ def get_from_secret_manager(project_id:str, secret_name:str):
     """Gets a secret from GCP Secret Manager."""
     client = secretmanager.SecretManagerServiceClient()
     request = {"name": f"projects/{project_id}/secrets/{secret_name}/versions/latest"}
-    response = client.access_secret_version(request.encode("UTF-8"))
+    response = client.access_secret_version(json.dumps(request).encode("UTF-8"))
     secret_string = response.payload.data.decode("UTF-8")
     return secret_string
 

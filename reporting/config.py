a6a12ce6b76ff5edfda59f334663a24b73d11aca
root <root@LT-GSHELBYDORMER.taos.local>
2020-11-09 11:04:28 -0500

    Add UTF-8 encoding on request to secret manager

--- reporting/config.py
+++ reporting/config.py
@@ -34,7 +34,7 @@ def get_from_secret_manager(project_id:str, secret_name:str):
     """Gets a secret from GCP Secret Manager."""
     client = secretmanager.SecretManagerServiceClient()
     request = {"name": f"projects/{project_id}/secrets/{secret_name}/versions/latest"}
-    response = client.access_secret_version(request)
+    response = client.access_secret_version(request.encode("UTF-8"))
     secret_string = response.payload.data.decode("UTF-8")
     return secret_string
 

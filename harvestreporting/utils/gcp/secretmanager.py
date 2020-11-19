b0b4fce47d5288ff62f92f5583e5161641e71f60
Gretchen Shelby-Dormer <gshelbydormer@taos.com>
2020-11-19 11:04:02 -0500

    Bump version `google-cloud-secret-manager`
    * Bump version `google-cloud-secret-manager` because the latest
    version works with our code and resolves an error where the
    secretmanager client wasn't able to access the secret

--- harvestreporting/utils/gcp/secretmanager.py
+++ harvestreporting/utils/gcp/secretmanager.py
@@ -7,6 +7,10 @@ def get_from_secret_manager(project_id:str, secret_name:str):
     """Gets a secret from GCP Secret Manager."""
     client = secretmanager.SecretManagerServiceClient()
     request = {"name": f"projects/{project_id}/secrets/{secret_name}/versions/latest"}
-    response = client.access_secret_version(json.dumps(request).encode("UTF-8"))
+    response = client.access_secret_version(request)
     secret_string = response.payload.data.decode("UTF-8")
     return secret_string
+
+
+if __name__ == "__main__":
+    bearer_token = get_from_secret_manager(project_id='dev-ops-now', secret_name='harvest-reports-bearer-token')
